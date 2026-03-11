61f75a5db3a136a214b27f75a7b8f59138691314
Killerkego <miklos.botos@gmail.com>
2026-03-11 14:28:57 +0100

    Modell osztályok

---
+++ Foldrengesek_soe/Models/Telepules.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Telepules
+{
+    public int ID { get; set; }
+    [Required]
+    [Display(Name = "Település név")]
+    public string Nev { get; set; } = null!;
+    [Required]
+    [Display(Name = "Vármegye")]
+    public string Varmegye { get; set; } = null!;
+
+    // Navigáció: településhez tartozó naplóbejegyzések lekéréséhez:
+    public virtual ICollection<Naplo>? Naplok { get; set; } = new List<Naplo>();
+}
