b8055bf680b06e213a3aed31b915f60b9345870f
Killerkego <miklos.botos@gmail.com>
2026-03-11 14:32:16 +0100

    Adat kontextus

---
+++ Foldrengesek_soe/Data/FoldrengesContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+public DbSet<Models.Telepules> Telepulesek { get; set; } = null!;
+public DbSet<Models.Naplo> Naplok { get; set; } = null!;
+
+public FoldrengesContext(DbContextOptions<FoldrengesContext> options) : base(options)
+{
+}
